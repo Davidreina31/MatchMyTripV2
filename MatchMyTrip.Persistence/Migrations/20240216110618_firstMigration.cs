8969cfdaf72ba79738be3392e551fe0832199092
Davidreina31 <Dave.reina31@gmail.com>
2024-02-16 12:18:22 +0100

    add EF core and first migration

---
+++ MatchMyTrip.Persistence/Migrations/20240216110618_firstMigration.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchMyTrip.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ActivityName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Filters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Destination = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NbrOfDays = table.Column<int>(type: "int", nullable: false),
+                    Seasons = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProfilePicture = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Journeys",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Destination = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NbrOfDays = table.Column<int>(type: "int", nullable: false),
+                    Seasons = table.Column<int>(type: "int", nullable: false),
+                    ProfileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Journeys", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Journeys_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profile_Filters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProfileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FilterId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profile_Filters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profile_Filters_Filters_FilterId",
+                        column: x => x.FilterId,
+                        principalTable: "Filters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Profile_Filters_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Journey_Activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    JourneyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ActivityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Journey_Activities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Journey_Activities_Activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Journey_Activities_Journeys_JourneyId",
+                        column: x => x.JourneyId,
+                        principalTable: "Journeys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Destination = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NbrOfDays = table.Column<int>(type: "int", nullable: false),
+                    Seasons = table.Column<int>(type: "int", nullable: false),
+                    MatchScore = table.Column<int>(type: "int", nullable: false),
+                    Favorite = table.Column<bool>(type: "bit", nullable: false),
+                    ProfileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    JourneyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Matches_Journeys_JourneyId",
+                        column: x => x.JourneyId,
+                        principalTable: "Journeys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Matches_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Match_Activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MatchId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ActivityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Match_Activities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Match_Activities_Activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Match_Activities_Matches_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "Matches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Match_Profiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MatchId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProfileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Match_Profiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Match_Profiles_Matches_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "Matches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Match_Profiles_Profiles_ProfileId",
+                        column: x => x.ProfileId,
+                        principalTable: "Profiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Journey_Activities_ActivityId",
+                table: "Journey_Activities",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Journey_Activities_JourneyId",
+                table: "Journey_Activities",
+                column: "JourneyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Journeys_ProfileId",
+                table: "Journeys",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Match_Activities_ActivityId",
+                table: "Match_Activities",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Match_Activities_MatchId",
+                table: "Match_Activities",
+                column: "MatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Match_Profiles_MatchId",
+                table: "Match_Profiles",
+                column: "MatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Match_Profiles_ProfileId",
+                table: "Match_Profiles",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_JourneyId",
+                table: "Matches",
+                column: "JourneyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_ProfileId",
+                table: "Matches",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profile_Filters_FilterId",
+                table: "Profile_Filters",
+                column: "FilterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profile_Filters_ProfileId",
+                table: "Profile_Filters",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Journey_Activities");
+
+            migrationBuilder.DropTable(
+                name: "Match_Activities");
+
+            migrationBuilder.DropTable(
+                name: "Match_Profiles");
+
+            migrationBuilder.DropTable(
+                name: "Profile_Filters");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "Matches");
+
+            migrationBuilder.DropTable(
+                name: "Filters");
+
+            migrationBuilder.DropTable(
+                name: "Journeys");
+
+            migrationBuilder.DropTable(
+                name: "Profiles");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ MatchMyTrip.Persistence/context/MatchMyTripDbContext.cs
@@ -0,0 +1,30 @@
+﻿using MatchMyTrip.Domain.entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchMyTrip.Persistence.context
+{
+    public class MatchMyTripDbContext : DbContext
+    {
+        public MatchMyTripDbContext(DbContextOptions<MatchMyTripDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Filter> Filters { get; set; }
+        public DbSet<Journey> Journeys { get; set; }
+        public DbSet<Journey_Activity> Journey_Activities { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<Match_Activity> Match_Activities { get; set; }
+        public DbSet<Match_Profile> Match_Profiles { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Profile_Filter> Profile_Filters { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
