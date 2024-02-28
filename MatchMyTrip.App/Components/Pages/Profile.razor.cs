adf7d486649ed602c3926806085d6c1097e9e80f
Davidreina31 <Dave.reina31@gmail.com>
2024-02-28 14:02:47 +0100

    add my profile to navbar

--- MatchMyTrip.App/Components/Pages/Profile.razor.cs
+++ MatchMyTrip.App/Components/Pages/Profile.razor.cs
@@ -1,6 +1,7 @@
 ﻿using MatchMyTrip.App.Interfaces;
 using MatchMyTrip.Application.features.activity.dto;
-using MatchMyTrip.Application.features.user.dto;
+using MatchMyTrip.Application.features.journey.dto;
+using MatchMyTrip.Application.features.user.dtos;
 using Microsoft.AspNetCore.Components;
 
 namespace MatchMyTrip.App.Components.Pages
@@ -10,14 +11,15 @@ namespace MatchMyTrip.App.Components.Pages
         [Inject]
         public IUserService UserService { get; set; }
 
-        public List<UserDTO> Users { get; set; } = new List<UserDTO>();
+        public UserQueryDTO User { get; set; } = new UserQueryDTO();
 
-        public UserDTO User { get; set; } = new UserDTO();
+        public List<JourneyDTO> Journeys { get; set; } = new List<JourneyDTO>();
 
         protected override async Task OnInitializedAsync()
         {
             var s = "DE1152A3-7DF4-4548-E134-08DC3452ABC2".ToLower();
             User = await UserService.GetUserById(Guid.Parse(s));
+            Journeys = User.Journeys;
         }
 
     }
