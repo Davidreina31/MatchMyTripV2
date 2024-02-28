70af62ea04994736caff2e9207beb959ca06347d
Davidreina31 <Dave.reina31@gmail.com>
2024-02-28 14:20:25 +0100

    add users management

---
+++ MatchMyTrip.App/Components/Pages/Users.razor.cs
@@ -0,0 +1,20 @@
+﻿using MatchMyTrip.App.Interfaces;
+using MatchMyTrip.Application.features.journey.dto;
+using MatchMyTrip.Application.features.user.dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace MatchMyTrip.App.Components.Pages
+{
+    public partial class Users
+    {
+        [Inject]
+        public IUserService UserService { get; set; }
+
+        public List<UserDTO> UserList { get; set; } = new List<UserDTO>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            UserList = await UserService.GetUsers();
+        }
+    }
+}
